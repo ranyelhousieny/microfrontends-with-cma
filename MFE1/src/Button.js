531c73164615f05522eecf7d257d2e176fa11c9b
Rany ElHousieny <elhousie@gmail.com>
2021-10-16 19:05:10 -0700

    Final

--- MFE1/src/Button.js
+++ MFE1/src/Button.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, {
+  useState,
+} from 'react';
 
-const Button = () => (
-  <button>MFE1 Button</button>
-);
+const Button = (props) => {
+  const [result, setResult] =
+    useState(null);
+
+  return (
+    <div>
+      <button
+        onClick={(e) => {
+          setResult(
+            props.value * 2
+          );
+        }}>
+        MFE1 Button
+      </button>
+      <span>{result}</span>
+    </div>
+  );
+};
+
 export default Button;
